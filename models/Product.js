fc62ba5bea3d3e5937cca579863211d1dfe5dd62
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-06-22 16:43:32 -0400

    added columns to product and tag models

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,37 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // NEED A VALIDATE HERE THAT THE VALUE IS A DECIMAL
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // NEED TO SET A DEFAULT VALUE OF 10
+      // NEED A VALIDATE THAT THE VALUE IS NUMERIC
+    },
+    category_id: {
+      type: DataTypes.INTEGER
+      // REFERENCES THE CATEGORY MODEL'S ID
+    }
   },
   {
     sequelize,
--- models/Tag.js
+++ models/Tag.js
@@ -2,11 +2,17 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
   },
   {
     sequelize,
